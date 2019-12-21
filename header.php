e7e75c77c91af7f830b2e3182a86d3d3b6da7f14
Yoana Stankova <yoana19st@gmail.com>
2019-12-21 17:45:45 +0200

    update

--- header.php
+++ header.php
@@ -1,26 +1,41 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+    body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+    .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+    .fa-anchor,.fa-coffee {font-size:200px}
+    </style>
     <link rel="stylesheet" type="text/css" href="main.css">
-    <title></title>
+
   </head>
   <body>
-    <header class="main_header" >
-      <a class = "bruh" href="#">language1</a> <!shte preprashta kum fail s sushtiq content ama na tozi ezik>
-      <a class = "bruh" href="#">language2</a> <! headera shte ostava sushtiq na vsichi ezici>
-      <a class = "bruh" href="#">language3</a>
-      <a class = "bruh" href="#">language4</a>
-      <a class = "bruh" href="#">language5</a>
-      <a class = "bruh" href="#">language6</a>
-  </header>
 
-  <div class="bruh" > <!Contenta na saita>
-    <a class = "bruh" href="#">Program</a> <!kotvi kum razlichnite chasti na saita>
-    <a class = "bruh" href="#">Fotos</a>
-    <a class = "bruh" href="#">Fotos</a>
-    <a class = "bruh" href="#">Fotos</a>
-    <a class = "bruh" href="#">Fotos</a>
-    <a class = "bruh" href="#">Fotos</a>
-    <a class = "bruh" href="#">Fotos</a>
-  </div>
+    <div class="w3-top">
+      <div class="w3-bar w3-red w3-card w3-left-align w3-large">
+        <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+        <a href="#" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
+        <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Germany</a>
+        <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Croatia</a>
+        <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Greece</a>
+        <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Austria</a>
+        <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Bulgaria</a>
+        <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Turkey</a>
+      </div>
+
+      <!-- Navbar on small screens -->
+      <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
+        <a href="#" class="w3-bar-item w3-button w3-padding-large">Germany</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding-large">Croatia</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding-large">Greece</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding-large">Austria</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding-large">Bulgaria</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding-large">Turkey</a>
+      </div>
+    </div>
--- index.php
+++ index.php
@@ -2,28 +2,100 @@
     require "header.php";
 ?>
 
-<a href="../link.html"><img src="img/germany-flag.png" style="width:82px; height:86px" title="Germany" alt="Germany"></a>
-<a href="../link.html"><img src="img/croatia-flag.png" style="width:82px; height:86px" title="Croatia" alt="Croatia"></a>
-<a href="../link.html"><img src="img/greece-flag.png" style="width:82px; height:86px" title="Greece" alt="Greece"></a>
-<a href="../link.html"><img src="img/austria-flag.png" style="width:82px; height:86px" title="Austria" alt="Austria"></a>
-<a href="../link.html"><img src="img/bulgaria-flag.png" style="width:82px; height:86px" title="Bulgaria" alt="Bulgaria"></a>
-<a href="../link.html"><img src="img/turkey-flag.png" style="width:82px; height:86px" title="Turkey" alt="Turkey"></a>
+<!-- Header -->
+<header class="w3-container w3-red w3-center" style="padding:128px 16px">
+  <h1 class="w3-margin w3-jumbo">Viefalt und Integration</h1>
+  <p class="w3-xlarge">American College of Sofia Erasmus 2019-2020</p>
+  <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Learn More</button>
+</header>
+
+<div class="row">
+ <div class="column">
+   <a href="../link.html"><img src="img/germany-flag.png" style="width:auto" title="Germany" alt="Germany"></a>
+ </div>
+ <div class="column">
+   <a href="../link.html"><img src="img/croatia-flag.png" style="width:auto" title="Croatia" alt="Croatia"></a>
+ </div>
+ <div class="column">
+   <a href="../link.html"><img src="img/greece-flag.png" style="width:auto" title="Greece" alt="Greece"></a>
+ </div>
+ <div class="column">
+   <a href="../link.html"><img src="img/austria-flag.png" style="width:auto" title="Austria" alt="Austria"></a>
+ </div>
+ <div class="column">
+   <a href="../link.html"><img src="img/bulgaria-flag.png" style="width:auto" title="Bulgaria" alt="Bulgaria"></a>
+ </div>
+ <div class="column">
+   <a href="../link.html"><img src="img/turkey-flag.png" style="width:auto" title="Turkey" alt="Turkey"></a>
+ </div>
+</div>
 
 <a href="../link.html"><img src="img/erasmus-logo.jpg" title="Erasmus" alt="Erasmus"></a>
 
-<div>
+<!-- First Grid -->
+<div class="w3-row-padding w3-padding-64 w3-container">
+  <div class="w3-content">
+    <div class="w3-twothird">
+      <h1>Lorem Ipsum</h1>
+      <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
 
-  <h3>add title</h3>
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </p>
+      <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
 
-  <h3>add title</h3>
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </p>
-
+    <div class="w3-third w3-center">
+      <i class="fa fa-anchor w3-padding-64 w3-text-red"></i>
+    </div>
+  </div>
 </div>
+
+<!-- Second Grid -->
+<div class="w3-row-padding w3-light-grey w3-padding-64 w3-container">
+  <div class="w3-content">
+    <div class="w3-third w3-center">
+      <i class="fa fa-coffee w3-padding-64 w3-text-red w3-margin-right"></i>
+    </div>
+
+    <div class="w3-twothird">
+      <h1>Lorem Ipsum</h1>
+      <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
+
+      <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+  </div>
+</div>
+
+<div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
+    <h1 class="w3-margin w3-xlarge">Quote of the day: live life</h1>
+</div>
+
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center w3-opacity">
+  <div class="w3-xlarge w3-padding-32">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+ </div>
+ <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+</footer>
+
+<script>
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+</script>
 
 
 <?php
