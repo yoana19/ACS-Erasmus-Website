5fd70c655b8a348da44ad9353b7b9cd4a52d297f
Yoana Stankova <yoana19st@gmail.com>
2019-12-21 16:48:48 +0200

    update

--- header.php
+++ header.php
@@ -7,14 +7,16 @@
   </head>
   <body>
     <header class="main_header" >
-      <a class = "bruh" href="#">language1</a> //shte preprashta kum fail s sushtiq content ama na tozi ezik
-      <a class = "bruh" href="#">language2</a> // headera shte ostava sushtiq na vsichi ezici
+      <a class = "bruh" href="#">language1</a> <!shte preprashta kum fail s sushtiq content ama na tozi ezik>
+      <a class = "bruh" href="#">language2</a> <! headera shte ostava sushtiq na vsichi ezici>
       <a class = "bruh" href="#">language3</a>
       <a class = "bruh" href="#">language4</a>
+      <a class = "bruh" href="#">language5</a>
+      <a class = "bruh" href="#">language6</a>
   </header>
 
-  <div class="bruh" > // Contenta na saita
-    <a class = "bruh" href="#">Program</a> / kotvi kum razlichnite chasti na saita
+  <div class="bruh" > <!Contenta na saita>
+    <a class = "bruh" href="#">Program</a> <!kotvi kum razlichnite chasti na saita>
     <a class = "bruh" href="#">Fotos</a>
     <a class = "bruh" href="#">Fotos</a>
     <a class = "bruh" href="#">Fotos</a>
--- index.php
+++ index.php
@@ -2,7 +2,29 @@
     require "header.php";
 ?>
 
-add html
+<a href="../link.html"><img src="img/germany-flag.png" style="width:82px; height:86px" title="Germany" alt="Germany"></a>
+<a href="../link.html"><img src="img/croatia-flag.png" style="width:82px; height:86px" title="Croatia" alt="Croatia"></a>
+<a href="../link.html"><img src="img/greece-flag.png" style="width:82px; height:86px" title="Greece" alt="Greece"></a>
+<a href="../link.html"><img src="img/austria-flag.png" style="width:82px; height:86px" title="Austria" alt="Austria"></a>
+<a href="../link.html"><img src="img/bulgaria-flag.png" style="width:82px; height:86px" title="Bulgaria" alt="Bulgaria"></a>
+<a href="../link.html"><img src="img/turkey-flag.png" style="width:82px; height:86px" title="Turkey" alt="Turkey"></a>
+
+<a href="../link.html"><img src="img/erasmus-logo.jpg" title="Erasmus" alt="Erasmus"></a>
+
+<div>
+
+  <h3>add title</h3>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+
+  <h3>add title</h3>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+
+</div>
+
 
 <?php
     require "footer.php";
